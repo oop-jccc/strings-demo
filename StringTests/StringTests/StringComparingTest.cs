1d95475dceeaaf138b951985e5e5d3b8806d0d06
Damon German <Damon.German@hyland.com>
2019-09-17 10:11:55 -0500

    made some changes

---
+++ StringTests/StringTests/StringComparingTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringTests
+{
+    [TestFixture]
+    public class StringComparingTest
+    {
+
+        [Test]
+        public void EqualsNullTest()
+        {
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                string string1 = null;
+                _ = string1.Equals("Hello World");
+            });
+        }
+
+        [Test]
+        public void EqualsTrueTest()
+        {
+            Assert.IsTrue("Hello World".Equals("Hello World"));
+        }
+
+        [Test]
+        public void EqualsFalseTest()
+        {
+            Assert.IsFalse("hello world".Equals("Hello World"));
+        }
+
+        [Test]
+        public void EqualsIgnoreCaseTest()
+        {
+            Assert.IsTrue("hello world".ToUpper().Equals("Hello World".ToUpper()));
+            Assert.IsTrue("hello world".Equals("Hello World",
+                StringComparison.InvariantCultureIgnoreCase));
+
+            Assert.IsTrue(" hello world   ".ToUpper().Trim().Equals("Hello World".ToUpper()));
+        }
+
+
+        [Test]
+        public void EqualityOperatorTest()
+        {
+            string string1 = null;
+            Assert.IsFalse(string1 == "Hello World");
+        }
+
+
+        [Test]
+        public void CompareToTest()
+        {
+            Assert.AreEqual("aaa".CompareTo("bbb"), -1);
+            Assert.AreEqual("aaa".CompareTo("aaa"), 0);
+            Assert.AreEqual("bbb".CompareTo("aaa"), 1);
+        }
+
+        [Test]
+        public void IntCompareToTest()
+        {
+            int CompareTo(int a, int b)
+            {
+                return a - b;
+            }
+
+            Assert.AreEqual(CompareTo(1, 2), -1);
+            Assert.AreEqual(CompareTo(1, 1), 0);
+            Assert.AreEqual(CompareTo(2, 1), 1);
+        }
+    }
+}
---
+++ StringTests/StringTests/StringConcatTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace StringTests
+{
+    [TestFixture]
+    public class StringConcatTests
+    {
+        private const string HelloWorld = "--Hello World!--";
+        private const string Hello = "Hello";
+        private const string World = "World";
+        private const string Exclamation = "!";
+
+        [Test]
+        public void StringConcatTest1()
+        {
+            const string testStr = "--" + Hello + " " + World + Exclamation + "--";
+            Assert.AreEqual(HelloWorld, testStr);
+        }
+
+        [Test]
+        public void StringConcatTest2()
+        {
+            var testStr = string.Concat("--", Hello, " ", World, Exclamation, "--");
+            Assert.AreEqual(HelloWorld, testStr);
+        }
+
+        [Test]
+        public void StringFormatTest()
+        {
+            var testStr = string.Format("--{0} {1}{2}--", Hello, World, Exclamation);
+            Assert.AreEqual(HelloWorld, testStr);
+        }
+
+        [Test]
+        public void InterpolationTest()
+        {
+            var testStr = $"--{Hello} {World}{Exclamation}--";
+            Assert.AreEqual(HelloWorld, testStr);
+        }
+
+        [Test]
+        public void StringBuilderTest()
+        {
+            var buffer = new StringBuilder("--");
+            buffer.Append("Hello ").Append("World!").Append("--");
+            var testStr = buffer.ToString();
+            Assert.AreEqual(HelloWorld, testStr);
+        }
+    }
+}

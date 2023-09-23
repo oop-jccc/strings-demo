d34de8ac7de7e39e3bec1e17e78a07b9868ad2bc
Damon German <damon.german@hyland.com>
2023-09-23 18:16:09 -0500

    Refactor code to latest syntactic conventions
    The C# code has been revamped in accordance with the latest syntax conventions by eliminating unnecessary curly brackets and redundant 'namespace' segments. Such clutter reduction enhances the code's readability and maintainability. C# 10.0 syntax uses a new semicolon-based syntax, rendering the code cleaner and more compact.

--- StringTests/StringTests/StartWithContainsTest.cs
+++ StringTests/StringTests/StartWithContainsTest.cs
@@ -1,26 +1,25 @@
 ﻿using NUnit.Framework;
 
-namespace StringTests
+namespace StringTests;
+
+[TestFixture]
+public class StartWithContainsTest
 {
-    [TestFixture]
-    public class StartWithContainsTest
+    [Test]
+    public void StartsWithTest()
     {
-        [Test]
-        public void StartsWithTest()
-        {
-            Assert.IsTrue("Hello".StartsWith("He"));
-        }
+        Assert.IsTrue("Hello".StartsWith("He"));
+    }
 
-        [Test]
-        public void EndsWithTest()
-        {
-            Assert.IsTrue("Hello".EndsWith("lo"));
-        }
+    [Test]
+    public void EndsWithTest()
+    {
+        Assert.IsTrue("Hello".EndsWith("lo"));
+    }
 
-        [Test]
-        public void ContainsTest()
-        {
-            Assert.IsTrue("Hello".Contains("lo"));
-        }
+    [Test]
+    public void ContainsTest()
+    {
+        Assert.IsTrue("Hello".Contains("lo"));
     }
 }
